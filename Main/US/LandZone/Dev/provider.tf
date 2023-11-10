98e09d6d657b87389f4e2c51891340016dcd4690
ksatishgit <106835928+ksatishgit@users.noreply.github.com>
2023-11-10 17:40:43 +0530

    Update provider.tf

--- Main/US/LandZone/Dev/provider.tf
+++ Main/US/LandZone/Dev/provider.tf
@@ -1,1 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+provider "azurerm" {
+ alias = "identity"
+ subscription_id = var.identity_subid
+ skip_provider_registration = true
+  features {}
+}
+
 
