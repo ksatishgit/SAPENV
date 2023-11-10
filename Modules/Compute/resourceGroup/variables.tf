270da51b94ae0c50ee4f15f6d5f96c99129ca829
ksatishgit <106835928+ksatishgit@users.noreply.github.com>
2023-11-10 17:55:11 +0530

    Update variables.tf

--- Modules/Compute/resourceGroup/variables.tf
+++ Modules/Compute/resourceGroup/variables.tf
@@ -1,1 +1,14 @@
+variable "resource_group_name" {
+ description = "Dont put anything here take it from global tfvars file"
+ type = string
+}
 
+variable "location" {
+ description = "Dont put anything here take it from global tfvars file"
+ type = string
+}
+
+variable  "tags" {
+ description = "tags will be mapped to resource froup"
+ type = map(string)
+}
