a965939515b4552665572085519f74e8c265621e
ksatishgit <106835928+ksatishgit@users.noreply.github.com>
2023-11-10 22:21:33 +0530

    Update main.tf

--- Main/US/LandZone/Dev/main.tf
+++ Main/US/LandZone/Dev/main.tf
@@ -1,6 +1,6 @@
 module "rg" {
  foreach           = var.resource_groups
- source            = "../../../../Modules/resourceGroup"
+ source            = "../../../../Modules/Compute/resourceGroup"
  resource_group_name  = each.value.name
  location          = var.location
  tags              = var.tags
