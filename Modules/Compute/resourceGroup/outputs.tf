a6e587a4f34d4f6cc86005f302e91e6bf89fa49c
ksatishgit <106835928+ksatishgit@users.noreply.github.com>
2023-11-10 17:58:30 +0530

    Update outputs.tf

--- Modules/Compute/resourceGroup/outputs.tf
+++ Modules/Compute/resourceGroup/outputs.tf
@@ -1,4 +1,12 @@
 output "rg_name" {
  description = "The name of this Resource Group"
- value = azurerm_resource_group.
+ value = azurerm_resource_group.firstrg.name
+}
+output "rg_location" {
+ description = "The name of this Resource Group"
+ value = azurerm_resource_group.firstrg.location
+}
+output "rg_id" {
+ description = "The name of this Resource Group"
+ value = azurerm_resource_group.firstrg.id
 }
