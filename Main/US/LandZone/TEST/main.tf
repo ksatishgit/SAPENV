5e5603449a3c8f130ca3a49571eace0580fc3701
ksatishgit <106835928+ksatishgit@users.noreply.github.com>
2023-11-18 15:25:19 +0530

    Update main.tf

--- Main/US/LandZone/TEST/main.tf
+++ Main/US/LandZone/TEST/main.tf
@@ -1,1 +1,8 @@
 
+module "rg" {
+ foreach           = var.resource_groups
+ source            = "../../../../Modules/Compute/resourceGroup"
+ resource_group_name  = each.value.name
+ location          = var.location
+ tags              = var.tags
+}
