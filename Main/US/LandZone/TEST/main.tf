2e28765e54f9c402eaaf9afa752506c428a4e43f
ksatishgit <106835928+ksatishgit@users.noreply.github.com>
2023-11-18 17:19:30 +0530

    Update main.tf

--- Main/US/LandZone/TEST/main.tf
+++ Main/US/LandZone/TEST/main.tf
@@ -1,4 +1,14 @@
-
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
 module "rg" {
  foreach           = var.resource_groups
  source            = "../../../../Modules/Compute/resourceGroup"
