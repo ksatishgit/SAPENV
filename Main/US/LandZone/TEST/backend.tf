c9c40c8b666ffcaabff8d7e456efd5721f3d1dc2
ksatishgit <106835928+ksatishgit@users.noreply.github.com>
2023-11-18 15:34:30 +0530

    Update backend.tf

--- Main/US/LandZone/TEST/backend.tf
+++ Main/US/LandZone/TEST/backend.tf
@@ -1,9 +1,9 @@
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "backend_rg"
-    storage_account_name = "statefilestore"
-    container_name       = "tfstate"
+    resource_group_name  = "remotebackend_rg"
+    storage_account_name = "remotefilestate"
+    container_name       = "remotetfstate"
     key                  = "resource.tfstate"
   }
 }
